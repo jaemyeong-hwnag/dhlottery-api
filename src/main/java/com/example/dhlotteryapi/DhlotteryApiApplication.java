e493f18259a3bdcba314847f835c7239e85e0135
jaemyeong-hwnag <woaudsla@gmail.com>
2023-04-23 23:02:30 +0900

    init

---
+++ src/main/java/com/example/dhlotteryapi/DhlotteryApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dhlotteryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhlotteryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DhlotteryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dhlotteryapi/DhlotteryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dhlotteryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhlotteryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
